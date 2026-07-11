26e2cbd03f5492f0085c5fc64acb97dca1794b86
Tejal <tejalpatil2002@gmail.com>
2026-07-11 22:20:35 +0530

    changing cors policy

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const app = express();
 
 
 // middlewares
-app.use(cors());
+app.use(cors(
+  {
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+  }
+));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/blog", blogRouter);
